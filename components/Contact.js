cbd9e78d43d5eded35562fca508ce89f2ba906f5
robertoooc <robertooo.c48@gmail.com>
2023-03-02 21:49:19 -0800

    added window relocation to contact info

--- components/Contact.js
+++ components/Contact.js
@@ -6,9 +6,9 @@ export default function Contact(){
         <div>
             <div className='text-center text-3xl font-medium'>Get in Touch</div>
             <div className="flex justify-center space-x-5">
-                <div className='bg-sky-700 rounded-full w-14 h-14 grid'><VscGithubInverted size={35} className='place-self-center'/></div>
-                <div className='bg-sky-700 rounded-full w-14 h-14 grid'><BsLinkedin size={35} className='place-self-center'/></div>
-                <div className='bg-sky-700 rounded-full w-14 h-14 grid'><MdOutlineMail size={35} className='place-self-center'/></div>
+                <div className='bg-sky-700 rounded-full w-14 h-14 grid' onClick={()=>window.open("https://github.com/robertoooc")}><VscGithubInverted size={35} className='place-self-center'/></div>
+                <div className='bg-sky-700 rounded-full w-14 h-14 grid' onClick={()=>window.open("https://www.linkedin.com/in/roberto-contreras-882737241/")}><BsLinkedin size={35} className='place-self-center'/></div>
+                <div className='bg-sky-700 rounded-full w-14 h-14 grid' onClick={()=>window.location="mailto:robertooo.c48@gmail.com"}><MdOutlineMail size={35} className='place-self-center'/></div>
             </div>
         </div>
     )
